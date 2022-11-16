7ce3db652852b5c11c797fee1d0c4b7f5d6a7ef3
nsjama15 <nsjama21@gmail.com>
2022-11-15 21:06:14 -0500

    All my npm packages are added/My server is up and running

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express() // set express to a variable express()
+const port = 3000 // set variable of port to 3000
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Pokemon App!")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`)
+});
